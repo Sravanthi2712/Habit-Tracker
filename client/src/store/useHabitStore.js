bf579be18c598f346c9ef82887b4e46083756b99
Sravanthi27 <lakkarajusravanthi@gmail.com>
2026-05-08 08:02:56 +0530

    added render link in place of localhost

--- client/src/store/useHabitStore.js
+++ client/src/store/useHabitStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import useAuthStore from './useAuthStore';
 
-const API_URL = 'http://localhost:5000/api/habits';
+const API_URL = 'http://habit-tracker-okab.onrender.com/api/habits';
 
 function getHeaders() {
   const token = useAuthStore.getState().session?.access_token;
